c50c0c73e1af26a7733ab0b23c7a5ceec76f9fcd
hardikgargok <hardikgargok@gmail.com>
2026-06-25 01:13:06 +0530

    Use StudyMeter logo for web app icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'studymeter-v8';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './studymeter-icon.svg'];
+const CACHE_NAME = 'studymeter-v9';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
