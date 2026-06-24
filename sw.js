351b1a16981f53bc6cd2ff2d5e1139c6dc7f36b1
hardikgargok <hardikgargok@gmail.com>
2026-06-25 00:20:45 +0530

    Add study toolkit and flashcard utilities

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studymeter-v6';
+const CACHE_NAME = 'studymeter-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './studymeter-icon.svg'];
 
 self.addEventListener('install', event => {
