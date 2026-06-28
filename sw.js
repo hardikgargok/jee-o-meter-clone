37828713cdb200a634fa5559b13b1eee5aadd768
hardikgargok <hardikgargok@gmail.com>
2026-06-28 23:34:05 +0530

    Add student launch command strip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studymeter-v19';
+const CACHE_NAME = 'studymeter-v20';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
