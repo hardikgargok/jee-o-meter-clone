e1b722074ff23119fbb64f286880acf89883a9b6
hardikgargok <hardikgargok@gmail.com>
2026-06-29 01:55:35 +0530

    Add second 100 improvement batch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studymeter-v30';
+const CACHE_NAME = 'studymeter-v31';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
