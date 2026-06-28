d161b12e588f0d491cb483d5981f78e6ea869f3f
hardikgargok <hardikgargok@gmail.com>
2026-06-29 01:02:23 +0530

    Add momentum levels and daily quests

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studymeter-v24';
+const CACHE_NAME = 'studymeter-v25';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
